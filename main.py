6b08812b1fa635a66f02fda9d13de7730dc9d23a
Braden Napier <brady.napier@dashos.net>
2020-08-08 18:41:42 -0400

    chore: add some commenting

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ async def main():
     config = APIConfig(
         api_key='<api_key>',
         api_secret='<api_secret>',
+        # These values are provided as arguments so that web3 won't have to be
+        # a dependency of the api wrapper itself.  This may be fine, not sure if 
+        # there are other packages that may be desired instead to do these things
         solidityKeccak=Web3.solidityKeccak,
         ethSignMessage=w3.eth.account.sign_message
     )
